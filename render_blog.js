001088e4068b046930c2ac8fff08cb88dd581826
Apiros3 <akikan.contact@gmail.com>
2023-08-19 01:02:43 +0900

    slight updates

--- render_blog.js
+++ render_blog.js
@@ -2,6 +2,6 @@
 function render_blog(path) {
     // console.log(path)
     document.getElementById("content").innerHTML = `
-        <iframe src="blog-html/${path}.html" title="${path}"></iframe>
+        <iframe src="blog-html/${path}.html" title="${path}" frameBorder="0"></iframe>
     `
 }
